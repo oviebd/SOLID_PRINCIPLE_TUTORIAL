d32e5cec421d38fc1003f2efaf315b391e92d78e
oviebd <oviecse48@gmail.com>
2021-01-17 09:09:21 +0600

    Update SRP

---
+++ Single Responsibility Principle/Assets/Scripts/Non SRP/PlayerController_Non_SRP.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_Non_SRP : MonoBehaviour
+{
+    [Header("Layers want to collided with")]
+    public LayerMask layerMask = new LayerMask();
+
+    private float speed = 6.0f;
+    private int currentHealth = 100;
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = new Vector3(moveHorizontal * (-1), 0.0f, moveVertical * (-1)) * speed * Time.deltaTime;
+
+        transform.position = transform.position + moveVector;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((layerMask.value & 1 << collision.gameObject.layer) != 0)
+        {
+            currentHealth = currentHealth - 5;
+            Debug.Log("Player Health " + currentHealth);
+        }
+    }
+
+}
---
+++ Single Responsibility Principle/Assets/Scripts/SRP/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    public static InputManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+            instance = this;
+    }
+
+    public float GetHorizontalAxisValue()
+    {
+        return Input.GetAxis("Horizontal");
+    }
+
+    public float GetVerticalAxisValue()
+    {
+        return Input.GetAxis("Vertical");
+    }
+
+}
